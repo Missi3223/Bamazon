b22b860a752e69be0295ff0405aeb48d12cbe54f
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-02-17 07:34:55 -0600

    Bamazon Customer Completed

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Bamazon
 Create tables with inventory information
+
+Instructions
+
+Customer View- 
+
+- [X] Create a MySQL Database called bamazon.
+- [X] Then create a Table inside of that database called products.
+The products table should have each of the following columns:
+
+- [X] item_id (unique id for each product)
+- [X] product_name (Name of product)
+- [X] department_name
+- [X] price (cost to customer)
+- [X] stock_quantity (how much of the product is available in stores)
+
+- [X] Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+- [X] Then create a Node application called bamazonCustomer.js. - [X] Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+The app should then prompt users with two messages.
+
+- [X] The first should ask them the ID of the product they would like to buy.
+- [X] The second message should ask how many units of the product they would like to buy.
+
+- [X] Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+- [X] If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+- [X] However, if your store does have enough of the product, you should fulfill the customer's order.
+
+- [X] Update the SQL database to reflect the remaining quantity.
+- [X] Once the update goes through, show the customer the total cost of their purchase.
+
+## Manager View (Next Level)
+
+Create a new Node application called bamazonManager.js. Running this application will:
+
+_List a set of menu options:_
+
+- [ ] View Products for Sale
+- [ ] View Low Inventory
+- [ ] Add to Inventory
+- [ ] Add New Product
+- [ ] If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
+- [ ] If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+- [ ] If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+- [ ] If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
---
+++ bamazonCustomer.js
@@ -0,0 +1,120 @@
+var mysql = require ('mysql');
+var inquirer = require ('inquirer');
+var Table = require ('cli-table');
+var chalk = require ('chalk');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon_db"
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("\n----------------------\n" +
+    " Welcome to Bamazon " +
+    "\n----------------------\n");
+
+    displayAll ();
+    
+});
+
+
+//   SELECT * FROM bamazon_db.products;
+function displayAll() {
+  //show all ids, names, and products from database.
+  connection.query('SELECT * FROM bamazon_db.products', function(error, response) {
+      if (error) { console.log(error) };
+      //New instance of our constructor
+      var theDisplayTable = new Table({
+          //declare the value categories
+          head:  ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity'],
+          //set widths to scale
+          colWidths: [10, 30, 18, 10, 14]
+      });
+      //for each row of the loop
+      for (i = 0; i < response.length; i++) {
+          //push data to table
+          theDisplayTable.push(
+              [response[i].item_id, response[i].product_name, response[i].department_name,"$"+ response[i].price, response[i].stock_quantity]
+          );
+      }
+      //log the completed table to console
+      console.log(theDisplayTable.toString());
+      
+      inquireForPurchase();
+    });
+
+
+}; //end displayAll
+function inquireForPurchase() {
+    //get item ID and desired quantity from user. Pass to purchase from Database
+    inquirer.prompt([
+
+        {
+            name: "ID",
+            type: "input",
+            message: "What is the item number for the product you wish to purchase?"
+        }, {
+            name: 'Quantity',
+            type: 'input',
+            message: "How many would you like to buy?"
+        },
+
+    ]).then(function(answers) {
+        //set captured input as variables, pass variables as parameters.
+        var quantityDesired = answers.Quantity;
+        var IDDesired = answers.ID;
+        purchaseFromDatabase(IDDesired, quantityDesired);
+    });
+
+}; 
+
+function purchaseFromDatabase(ID, quantityNeeded) {
+    //check DB to see if the quatity is available. If available subtract the amount ordered for DB.  If unavailable the let user know 
+    connection.query('SELECT * FROM bamazon_db.products WHERE item_id = ' + ID, function(error, response) {
+        if (error) { console.log(error) };
+
+        //if in stock
+        if (quantityNeeded <= response[0].stock_quantity) {
+            //calculate cost
+            var totalCost = response[0].price * quantityNeeded;
+            //inform user
+            console.log("We have you covered! ");
+            console.log("Your total cost for " + quantityNeeded + " " + response[0].product_name + " is $" + totalCost + ". Thank you for shopping at Bamazon!");
+            //update database, minus purchased quantity
+            connection.query('UPDATE bamazon_db.products SET stock_quantity = stock_quantity - ' + quantityNeeded + ' WHERE item_id = ' + ID);
+        } else {
+            console.log("OH NO!!!!  We are sold out of " + response[0].product_name + " and are unable to fulfill your order.");
+        };
+        continueShopping();
+    });
+};
+function continueShopping(){
+    inquirer.prompt(
+        {
+        type: "input",
+        name:"response",
+        message: "Are you done shopping for today?",
+        choices: ["Yes", "No"]
+        })
+
+       .then(function(answers){
+           
+        
+       if (answers.response === "No")
+            displayAll();
+           
+            
+       })
+       
+    };
+    
+ 
